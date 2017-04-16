ddf4ec48e17c601350c3824e2cc8aefd46dc327a
Xiaoliang Wu <zurielibra@gmail.com>
2017-04-16 08:19:02 -0500

    start hw6

---
+++ HW6/HW6-News-Gateway/app/src/main/java/edu/iit/xwu64/hw6_news_gateway/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.iit.xwu64.hw6_news_gateway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
